d9211846a83098a8d64034e6bf4c1b73273e2da1
Leonardosouza17 <leonardompdesouza@gmail.com>
2024-04-11 11:48:47 -0300

    atualizando e estilizando

--- tema.js
+++ tema.js
@@ -1,9 +1,16 @@
-document.querySelector('#btTrocaTema').addEventListener('click', function trocaTema() {
+document.querySelector('#tema-escuro').addEventListener('click', function trocaTema() {
     
     if (document.documentElement.getAttribute('data-tema') === 'light') {
         document.documentElement.setAttribute('data-tema', 'dark');
 
-    } else {
-        document.documentElement.setAttribute('data-tema', 'light');
     }
 });
+
+document.querySelector('#tema-claro').addEventListener('click', function trocaTema() {
+    
+    if (document.documentElement.getAttribute('data-tema') === 'dark') {
+        document.documentElement.setAttribute('data-tema', 'light');
+
+    }
+});
+
