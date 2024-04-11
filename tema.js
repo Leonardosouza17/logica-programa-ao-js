e663479b3b68ceb056cc532bf41eef468c3d2b5e
Leonardosouza17 <leonardompdesouza@gmail.com>
2024-04-11 19:43:54 -0300

    Style CSS

--- tema.js
+++ tema.js
@@ -1,16 +1,12 @@
-document.querySelector('#tema-escuro').addEventListener('click', function trocaTema() {
-    
-    if (document.documentElement.getAttribute('data-tema') === 'light') {
+document.querySelector('#btTrocaTema').addEventListener('click', 
+function trocaTema() {
+    // pegar o elemento com o tema
+    // se o tema for light, então mude o atributo para dark
+    // se não for, então mude o atributo para light
+    if (document.documentElement.getAttribute('data-tema') === 
+    'light') {
         document.documentElement.setAttribute('data-tema', 'dark');
-
-    }
-});
-
-document.querySelector('#tema-claro').addEventListener('click', function trocaTema() {
-    
-    if (document.documentElement.getAttribute('data-tema') === 'dark') {
+    } else {
         document.documentElement.setAttribute('data-tema', 'light');
-
     }
 });
-
