d3b1799b1ee405c52ed81a404e87a73b364537e3
Leonardosouza17 <leonardompdesouza@gmail.com>
2024-05-06 11:46:05 -0300

    style: adicionando estilos

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -1,26 +1,72 @@
 // Ancora do exercício 1
 
-const aHome = criaAncora('../html/index.html', 'Home');
+const ancoras = [
+    { "url": "/index.html", "texto": "Home" },
+    { "url": "../html/exercicio1.html", "texto": "Exerc. 1" },
+    { "url": "../html/exercicio2.html", "texto": "Exerc. 2" },
+    { "url": "../html/exercicio3.html", "texto": "Exerc. 3" },
+    { "url": "../html/exercicio4.html", "texto": "Exerc. 4" },
+    { "url": "../html/exercicio5.html", "texto": "Exerc. 5" },
+    { "url": "../html/teste6.html", "texto": "Exerc. 6" },
+    { "url": "../html/exercicio7.html", "texto": "Exerc. 7" }
+];
 
-const liHome = document.createElement('li');
-liHome.appendChild(aHome);
+function criaAncora(url, texto) {
 
-const aExercicio1 = criaAncora('../html/exercicio1.html' , 'Exercicio 1');
+    const ancora = document.createElement('a');
+    ancora.setAttribute('href', url);
+    ancora.innerHTML = texto;
 
-const liExercicio1 = criaLi(aExercicio1);
+    return ancora;
 
-const aExercicio6 = criaAncora('../html/teste6.html' , 'Exercicio 6');
+}
 
 
-const liExercicio6 = document.createElement('li');
-liExercicio6.appendChild(aExercicio6);
+function criaLi(ancora) {
 
-const ulNav = document.createElement('ul')
-ulNav.append(liHome, liExercicio1, liExercicio6);
+    const li = document.createElement('li');
+    li.appendChild(ancora);
+
+    return li;
+}
+
+function criaUlNav (ancoras) {
+
+    const ul = document.createElement('ul');
+
+    ancoras.forEach(ancora => {
+        const a = criaAncora(ancora.url, ancora.texto);
+        const li = criaLi(a);
+        ul.appendChild(li);
+    });
+
+    return ul;
+        
+
+}
+
+// const aHome = criaAncora('../html/index.html', 'Home');
+
+// const liHome = document.createElement('li');
+// liHome.appendChild(aHome);
+
+// const aExercicio1 = criaAncora('../html/exercicio1.html', 'Exercicio 1');
+
+// const liExercicio1 = criaLi(aExercicio1);
+
+
+
+// const aExercicio6 = criaAncora('../html/teste6.html', 'Exercicio 6');
+
+// const liExercicio6 = document.createElement('li');
+// liExercicio6.appendChild(aExercicio6);
+
+// const ulNav = document.createElement('ul')
+// ulNav.append(liHome, liExercicio1, liExercicio6, liExercicio5);
 
 const nav = document.createElement('nav');
 nav.classList.add('menu-navegacao');
-nav.appendChild(ulNav);
+nav.appendChild(criaUlNav(ancoras));
 
 const spanLogo = document.createElement('span');
 spanLogo.innerHTML = 'Desafios & Exercícios';
@@ -50,22 +96,3 @@ linkNavbar.setAttribute('href', '../style/navbar.css')
 
 document.head.appendChild(linkNavbar);
 
-function criaAncora(url, texto) {
-
-    const ancora = document.createElement('a');
-    ancora.setAttribute('href', url);
-    ancora.innerHTML = texto;
-
-    return ancora;
-
-}
-
-// Crie uma função e utilize para gerar os elementos do tipo <li>
-
-function  criaLi(ancora) {
-
-    const li = document.createElement('li');
-    li.appendChild(ancora);
-
-    return li;
-}
